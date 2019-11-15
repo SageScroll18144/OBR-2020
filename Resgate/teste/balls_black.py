dafc23d7e4a468375de7457f8b46adda9f3083c6
SageScroll18144 <lipe.fssantos063@gmail.com>
2019-11-15 15:06:05 -0300

    Implementado o algortimo para detecção de vitimas pretas. O algortimo só detecta uma por vez

---
+++ Resgate/teste/balls_black.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+PATH = '/home/felipe/Imagens/ball.png'  
+
+frame = cv2.imread(PATH)
+img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 260, param1=30, param2=65, minRadius=0, maxRadius=0)
+
+if circles is not None:
+    for x, y, r in circles[0]:
+        cv2.circle(frame,(x,y),r,(0,255,0),2)
+
+cv2.imshow('Xamaa',frame)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
