e738af10a2424b60501039e00a0452589c24fe71
SageScroll18144 <lipe.fssantos063@gmail.com>
2019-12-27 15:09:57 -0300

    Ultrassonico implementado

--- Resgate/main/main.py
+++ Resgate/main/main.py
@@ -1,14 +1,18 @@
 import RPi.GPIO as GPIO
 import time 
+import ultrassonic as ult
 
 GPIO.setmode(GPIO.BCM)
 
-trig = 1
-echo = 0
+ult_sen = [0,1] #trig[0], echo[0], trig[1], echo[1], ... , trig[n], echo[n]
 
-GPIO.setup(trig,GPIO.OUT)
-GPIO.setup(echo,GPIO.IN)
+for i in range(len(ult_sen)):
+	if((i%2) == 0):
+		GPIO.setup(ult_sen[i], GPIO.OUT)
+	else:
+		GPIO.setup(ult_sen[i], GPIO.IN)
 
-distance = ult.read(trig, echo)
+#ex.:
+distance = ult.read(ult_sen[0], ult_sen[1])
 
 GPIO.cleanup()
