610a85fc69b8026fa6f18dfddfd0352225aa93c0
Felipe Santos <lipe.fssantos063@gmail.com>
2020-04-16 17:34:03 -0300

    right and left'

--- Seguidor-Desvio/main_b/move.ino
+++ Seguidor-Desvio/main_b/move.ino
@@ -9,15 +9,33 @@ void forward(){
 }
 void backward(){
   for(int i = 0; i < sizeof(motores)/sizeof(int); i++){
-    digitalWrite(motores[i*2], 1);
-    digitalWrite(motores[i*2+1], 0);
+    digitalWrite(motores[i*2], 0);
+    digitalWrite(motores[i*2+1], 1);
+  }
+}
+void left(){
+  for(int i = 0; i < sizeof(motores)/sizeof(int); i++){
+    if((i%2)==0){
+      digitalWrite(motores[i*2], 0);
+      digitalWrite(motores[i*2+1], 1);
+    }
+    else{
+      digitalWrite(motores[i*2], 1);
+      digitalWrite(motores[i*2+1], 0);
+    }
   }
 }
 void right(){
-  
-}
-void left(){
-  
+  for(int i = 0; i < sizeof(motores)/sizeof(int); i++){
+    if((i%2)==0){
+      digitalWrite(motores[i*2], 1);
+      digitalWrite(motores[i*2+1], 0);
+    }
+    else{
+      digitalWrite(motores[i*2], 0);
+      digitalWrite(motores[i*2+1], 1);
+    }
+  }
 }
 void spin_l(){
   
