05276393cfccedb4545f50548aa66033ea7c4f5d
Felipe Santos <lipe.fssantos063@gmail.com>
2020-04-16 17:00:14 -0300

    init first moviments

--- robo_rasp/motors.py
+++ robo_rasp/motors.py
@@ -1,3 +1,32 @@
 import RPi.GPIO as GPIO
 import time    
+import numpy as np
+
+'''
+ M0  ####  M3.
+    ######       -> chassi do robô
+    ######       -> M(motor)[número do motor]
+ M1  ####  M2
+'''
+
+class Control:
+    def __init__(self, motors, directions):
+        self.motors = motors
+        self.directions = directions
+    def print_motors(self):
+        for i in motors:
+            print(i)
+    def forward(self):
+        for i in motors:
+            GPIO.output(directions[i*2], 1)
+            GPIO.output(directions[i*2+1], 0)
+    def backward(self):
+        for i in motors:
+            GPIO.output(directions[i*2], 0)
+            GPIO.output(directions[i*2+1], 1)
+
+motors = np.array([1,1,1])
+directions = np.array([1,1,1])
+control = Control(motors, directions)
+control.print_motors()
 
