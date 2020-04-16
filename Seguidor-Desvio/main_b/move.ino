00fe04567efec7f424506c039a56ee3a9228e3f2
Felipe Santos <lipe.fssantos063@gmail.com>
2020-04-16 17:29:19 -0300

    forward and backward

--- Seguidor-Desvio/main_b/move.ino
+++ Seguidor-Desvio/main_b/move.ino
@@ -2,10 +2,16 @@ void set_velocity(int motor, int vel){
   analogWrite(motores[motor], vel);
 }
 void forward(){
-  
+  for(int i = 0; i < sizeof(motores)/sizeof(int); i++){
+    digitalWrite(motores[i*2], 1);
+    digitalWrite(motores[i*2+1], 0);
+  }
 }
 void backward(){
-  
+  for(int i = 0; i < sizeof(motores)/sizeof(int); i++){
+    digitalWrite(motores[i*2], 1);
+    digitalWrite(motores[i*2+1], 0);
+  }
 }
 void right(){
   
